b36e5a84f286960df9413797d8a817f2345d7241
YamamotoDesu <mynameisyamamotodesu@gmail.com>
2024-05-06 18:34:57 +0900

    Initial Commit

---
+++ UseConfigurationSettingsFiles/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UseConfigurationSettingsFiles
+//
+//  Created by Yamamoto Kyo on 2024/05/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UseConfigurationSettingsFiles/UseConfigurationSettingsFilesApp.swift
@@ -0,0 +1,17 @@
+//
+//  UseConfigurationSettingsFilesApp.swift
+//  UseConfigurationSettingsFiles
+//
+//  Created by Yamamoto Kyo on 2024/05/06.
+//
+
+import SwiftUI
+
+@main
+struct UseConfigurationSettingsFilesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
